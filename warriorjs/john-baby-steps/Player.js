26d83b3e513ca4e5ff6046a95ebcf80ad4f2245e
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-08-01 22:30:02 +0200

    Epic - All on level A or S

--- warriorjs/john-baby-steps/Player.js
+++ warriorjs/john-baby-steps/Player.js
@@ -2,6 +2,7 @@ class Player {
 
   constructor() {
     this.hasTracedBack = false;
+    this.hasHitWall = false;
     this.isFacingFoward = true;
     this.healingUp = false;
     this.vision = [];
@@ -29,9 +30,11 @@ class Player {
     if (!this.hasTracedBack && this.isFacingFoward) {
       if (this.warrior.feel('backward').isWall()) {
         this.hasTracedBack = true;
+        this.hasHitWall = true;
         return false;
       }
       this.turnAround();
+      this.hasTracedBack = true;
       return true;
     }
   }
@@ -41,15 +44,6 @@ class Player {
   }
 
   handleVision(index, vision) {
-    if (vision.isStairs()) {
-      this.warrior.walk();
-      return true;
-    }
-    if (vision.isEmpty()) return false; // Stairs count as empty as well
-    if (vision.isWall()) {
-      this.turnAround();
-      return true;
-    }
     if (vision.isUnit()) {
       const unit = vision.getUnit();
       if (unit.isBound()) {
@@ -64,66 +58,29 @@ class Player {
       this.warrior.shoot();
       return true;
     }
+    if (vision.isStairs()) {
+      if (!this.hasHitWall) {
+        this.turnAround();
+        return true;
+      }
+      this.warrior.walk();
+      return true;
+    }
+    if (vision.isEmpty()) return false; // Stairs count as empty as well
+    if (vision.isWall()) {
+      this.turnAround();
+      this.hasHitWall = true;
+      return true;
+    }
   }
 
   updateHealth() {
     this.health = this.warrior.health();
   }
 
-  isSomethingThere() {
-    return !this.warrior.feel(this.getDirection()).isEmpty();
-  }
-
-  isToughEnough() {
-    if (this.healingUp) {
-      if (this.warrior.health() == 20) this.healingUp = false;
-      return !this.healingUp;
-    }
-    return this.warrior.health() >= 10;
-  }
-
-  rest() {
-    this.warrior.rest();
-    this.healingUp = true;
-  }
-
-  isTakingDamage() {
-    return this.health > this.warrior.health();
-  }
-
-  fallBack() {
-    this.warrior.walk('backward');
-  }
-
   turnAround() {
     this.isFacingFoward = !this.isFacingFoward;
     this.warrior.pivot();
-  }
-
-  handleFeeling(feel) {
-    if (feel.isWall())  {
-      this.warrior.think("Turn around!");
-      this.direction = 'forward';
-      this.hasTracedBack = true;
-      this.warrior.walk(this.getDirection());
-    } else {
-      const unit = feel.getUnit();
-      if (unit.isBound()) {
-        this.warrior.think("To the rescue!");
-        this.warrior.rescue(this.getDirection());
-      }  else {
-        this.warrior.think("I will take you out!")
-        this.warrior.attack(this.getDirection());
-      }
-    }
-  }
-
-  isToughEnoughToRunAndFaceArcher() {
-    return this.warrior.health() > 9;
-  }
-
-  getDirection() {
-    return 'forward';
   }
 }
 
