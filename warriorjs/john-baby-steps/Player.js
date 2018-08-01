1663acd589a67c0848c2a8e62fae2cd100d607ba
John Hotterbeekx <john.online@hotterbeekx.nl>
2018-08-01 19:14:18 +0200

    Level 4 - Finished

--- warriorjs/john-baby-steps/Player.js
+++ warriorjs/john-baby-steps/Player.js
@@ -1,23 +1,51 @@
 class Player {
   playTurn(warrior) {
-    if (this.isFacingEnemy(warrior)) {
+    this.setWarrior(warrior);
+
+    if (this.isFacingEnemy()) {
+      warrior.think('Die you bastard!');
       warrior.attack();
     }
     else {
-      if (!this.isToughEnough(warrior)) {
-        warrior.rest();
-      } else {
+      if(this.isTakingDamage()) {
+        warrior.think('There is an archer, have to take him out!');
         warrior.walk();
+      } else {
+        if (!this.isToughEnough()) {
+          warrior.think('I\'m feeling a bit weak...');
+          warrior.rest();
+        } else {
+          warrior.think('Taking a walk on the wild side...');
+          warrior.walk();
+        }
       }
     }
+    this.updateHealth();
   }
 
-  isFacingEnemy(warrior) {
-    return !warrior.feel().isEmpty();
+  setWarrior(warrior) {
+    this.warrior = warrior;
   }
 
-  isToughEnough(warrior) {
-    return warrior.health() > 6;
+  updateHealth() {
+    this.health = this.warrior.health();
+  }
+
+  isFacingEnemy() {
+    return !this.warrior.feel().isEmpty();
+  }
+
+  isToughEnough() {
+    return this.warrior.health() > 12;
+  }
+
+  isTakingDamage() {
+    return this.health > this.warrior.health();
+  }
+
+  isToughEnoughToRunAndFaceArcher() {
+    return this.warrior.health() > 9;
   }
 }
+
 
